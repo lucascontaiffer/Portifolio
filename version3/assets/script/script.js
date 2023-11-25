2655e7ac35207dcc845c830e63d48eec1e5cc137
lucas <134218178+lucascontaiffer@users.noreply.github.com>
2023-11-25 12:35:47 -0800

    Add files via upload

--- version3/assets/script/script.js
+++ version3/assets/script/script.js
@@ -1,3 +1,30 @@
+/* SWIPER */
+let swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints:{
+    0:{
+      slidesPerView: 1,
+    },
+    767:{
+      slidesPerView: 2,
+    },
+    1024:{
+      slidesPerView: 3,
+    }
+  },
+});
+
+/*CONTACT BTN*/
 const copyBtn = document.querySelector('.contact-btn')
 const eMail = "lucasdacostacontaiffer@gmail.com" 
 
